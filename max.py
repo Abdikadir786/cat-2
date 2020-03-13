14b28fd33c3e09a91783e965b93b79d826aa83ef
Abdikadir786 <60694895+Abdikadir786@users.noreply.github.com>
2020-03-14 01:43:09 +0300

    Add files via upload

---
+++ max.py
@@ -0,0 +1,16 @@
+def maximum(a, b, c):
+    if (a >= b) and (a >= b):
+        largest = a
+
+    elif (b >= a) and (b >= a):
+        largest = b
+    else:
+        largest = c
+
+    return largest
+
+
+a = 20
+b = 84
+c = 70
+print(maximum(a, b, c))
